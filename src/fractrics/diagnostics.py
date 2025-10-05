4e48be4184b45562c74a23aa36e3e46a5b803cd1
Alfredo Diodati <diodatialfredo@gmail.com>
2025-10-05 12:23:28 +0200

    implemented garchml

--- src/fractrics/diagnostics.py
+++ src/fractrics/diagnostics.py
@@ -34,3 +34,9 @@ def tail_pv(ps1, ps2, order=[1, 5, 7 , 10]):
     
     nabsZ = -jnp.abs((ps1 - ps2)*jnp.sqrt(order/2))
     return 2 * jss.norm.cdf(nabsZ, loc=0, scale=1)
+
+def AIC(log_likelihood:float, n_params:int) -> float:
+    return 2 * n_params - 2 * log_likelihood
+
+def BIC(log_likelihood:float, n_params:int, n_observations:int) -> float:
+    return n_params * jnp.log(n_observations) - 2 * log_likelihood
